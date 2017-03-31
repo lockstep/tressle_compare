0630de7c705e9a230dd831a3103a806bf3a41f2f
birdytripler <birdy.tripler@gmail.com>
2017-03-31 15:38:01 +0700

    Enhance UI rely on new database design

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,14 @@ class Product < ApplicationRecord
 
   scope :search, -> (query) {
     where(<<-SQL, "%#{query}%", "%#{query}%")
-      name ILIKE ? OR description ILIKE ?
+      products.name ILIKE ? OR
+      products.description ILIKE ?
     SQL
+  }
+
+  scope :with_comparisons, -> {
+    joins('LEFT OUTER JOIN comparisons ON products.id = comparisons.product_id')
+    .joins('INNER JOIN retailers ON retailers.id = comparisons.retailer_id')
+    .select('products.*, comparisons.current_price, retailers.name AS retailer_name')
   }
 end
