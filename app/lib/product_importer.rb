fb2fe5791824fd09b8a271c71ed8b58ec71e27ed
birdytripler <birdy.tripler@gmail.com>
2017-04-10 11:14:55 +0700

    Adjust importer script

--- app/lib/product_importer.rb
+++ app/lib/product_importer.rb
@@ -22,32 +22,36 @@ module ProductImporter
       product = Product.where(external_url: item['url'].split('?').first)
         .first_or_initialize
       product.update({
-        name: extract_data(item['name']) || product.name,
-        manufacturer: extract_data(item['manufacturer']) || product.manufacturer,
-        manufacturer_sku: extract_data(item['manufacturer_sku']) || product.manufacturer_sku,
-        description: extract_data(item['description']) || product.description,
-        sku: extract_data(item['sku']) || product.sku,
-        image_url: extract_data(item['primary_image_url']) || product.image_url,
-        original_price: extract_data(item['original_price']) || product.original_price,
-        current_price: extract_data(item['current_price']) || product.current_price,
-        average_rating: extract_data(item['average_rating']) || product.average_rating,
-        ratings_count: extract_data(item['ratings_count']) || product.ratings_count,
-        primary_category: extract_data(item['primary_category']) || product.primary_category,
-        secondary_category: extract_data(item['secondary_category']) || product.secondary_category,
-        tertiary_category: extract_data(item['tertiary_category']) || product.tertiary_category,
-        color: extract_data(item['color']) || product.color,
-        material: extract_data(item['material']) || product.material,
+        name: extract_data(item, 'name') || product.name,
+        manufacturer: extract_data(item, 'manufacturer') || product.manufacturer,
+        manufacturer_sku: extract_data(item, 'manufacturer_sku') || product.manufacturer_sku,
+        description: extract_data(item, 'description') || product.description,
+        sku: extract_data(item, 'sku') || product.sku,
+        image_url: extract_data(item, 'primary_image_url') || product.image_url,
+        original_price: extract_data(item, 'original_price') || product.original_price,
+        current_price: extract_data(item, 'current_price') || product.current_price,
+        average_rating: extract_data(item, 'average_rating') || product.average_rating,
+        ratings_count: extract_data(item, 'ratings_count') || product.ratings_count,
+        primary_category: extract_data(item, 'primary_category') || product.primary_category,
+        secondary_category: extract_data(item, 'secondary_category') || product.secondary_category,
+        tertiary_category: extract_data(item, 'tertiary_category') || product.tertiary_category,
+        color: extract_data(item, 'color') || product.color,
+        material: extract_data(item, 'material') || product.material,
       })
     end
   end
 
-  def self.extract_data(result)
-    if result && result.is_a?(Array)
-      return result.first[1..-1] if result.first[0] == '$'
-      return result.first
+  def self.extract_data(item, field)
+    return if item[field].blank?
+    result = item[field].is_a?(Array) ? item[field].first : item[field]
+    return if result.blank?
+    if field =~ /price/
+      result.sub!(',', '')
+      return result[1..-1] if result[0] == '$'
+      return result
+    elsif field == 'sku' && result =~ /SKU/i
+      return result.scan(/SKU: (.*)/i).join
     else
-      return result.scan(/SKU: (.*)/i).join if result =~ /SKU/i
-      return nil if result.blank?
       result
     end
   end
