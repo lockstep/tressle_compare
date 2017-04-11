25b9c677dea33f307070f92205d80b6835dad09b
birdytripler <birdy.tripler@gmail.com>
2017-04-11 15:56:41 +0700

    Amend as feedback

--- spec/features/product_page_spec.rb
+++ spec/features/product_page_spec.rb
@@ -2,14 +2,20 @@ require 'rails_helper'
 
 feature 'Product page' do
   context 'product exists' do
-    before do
-      @product = create(:product, current_price: 1000)
-    end
-    scenario "user can navigate to a retailer's product site" do
+    before { @product = create(:product) }
+
+    scenario "user sees product detail" do
       visit products_path
       expect(page).to have_content 'Study Desk'
       click_on 'Study Desk'
       expect(page).to have_content @product.description
+    end
+
+    scenario "user can navigate to a retailer's product site" do
+      visit products_path
+      expect(page).to have_content 'Study Desk'
+      click_on 'Study Desk'
+      expect(page).to have_link('BUY', href: @product.external_url)
     end
   end
 end
--- spec/features/search_page_spec.rb
+++ spec/features/search_page_spec.rb
@@ -20,44 +20,6 @@ feature 'Search page' do
         expect(page).to have_content 'Study Desk'
         expect(page).to have_content 'Red Cabinet'
       end
-
-      scenario 'user can sort products by name ascending' do
-        visit products_path
-        click_link_or_button 'Sort by'
-        all('.sort-product .dropdown-item')[0].click
-        within first('.product') do
-          expect(page).to have_content 'Red Cabinet'
-        end
-      end
-
-      scenario 'user can sort products by name descending' do
-        visit products_path
-        click_link_or_button 'Sort by'
-        all('.sort-product .dropdown-item')[1].click
-        within first('.product') do
-          expect(page).to have_content 'Study Desk'
-        end
-      end
-
-      scenario 'user can sort products by price ascending' do
-        visit products_path
-        click_link_or_button 'Sort by'
-        all('.sort-product .dropdown-item')[2].click
-        within first('.product') do
-          expect(page).to have_content 'Study Desk'
-          expect(page).to have_content '$123.45'
-        end
-      end
-
-      scenario 'user can sort products by price descending' do
-        visit products_path
-        click_link_or_button 'Sort by'
-        all('.sort-product .dropdown-item')[3].click
-        within first('.product') do
-          expect(page).to have_content 'Red Cabinet'
-          expect(page).to have_content '$1,000.00'
-        end
-      end
     end
   end
 
