5d3528bbc5af705f2b10f4d408b278a3878c7ed2
sasitornamy <sasitorn.oeam@gmail.com>
2017-04-12 16:36:51 +0700

    added shopping list page
    * added shopping list page
    
    * Random products to show in cart

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products
   get 'categories', to: 'categories#index'
+  get 'carts', to: 'shopping_carts#index'
 
   root to: 'home#index'
 end
