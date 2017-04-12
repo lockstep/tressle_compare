d387a5ef21bd2255067166124c100aad8ba59dec
Paul Hiatt <hiattp@gmail.com>
2017-04-12 12:31:40 +0700

    added basic auth to production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
+  if Rails.env.production?
+    http_basic_authenticate_with name: "tressle", password: "T2017"
+  end
   protect_from_forgery with: :exception
 end
