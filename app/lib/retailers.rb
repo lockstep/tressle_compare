b8b8e3526be6597107bc8368685e15b9d7f9f6dd
Paul Hiatt <hiattp@gmail.com>
2017-04-18 14:00:04 +0700

    added logo images

--- app/lib/retailers.rb
+++ app/lib/retailers.rb
@@ -5,4 +5,16 @@ module Retailers
     'hayneedle' => 'Hayneedle',
     'allmodern' => 'AllModern'
   }
+
+  IMAGE_PATH_BY_NAME = {
+    'Wayfair' => 'retailers/10.png',
+    'Houzz' => 'retailers/4.png',
+    'Hayneedle' => 'retailers/3.png',
+    'AllModern' => 'retailers/1.png',
+    'Overstock' => 'retailers/overstock.png',
+    'Designer Living' => 'retailers/designer_living.png',
+    'National Furniture Supply' => 'retailers/national_furniture_supply.png',
+    'Bed Bath & Beyond' => 'retailers/bed_bath_and_beyond.png',
+    'Target' => 'retailers/target.png'
+  }
 end
